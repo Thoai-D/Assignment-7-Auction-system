e5360a8bfe15f72e6d522952a97ea8f4eb94fe75
Thoai Do Huy <thoaidohuy@gmail.com>
2021-01-25 12:15:01 +0700

    Add project files.

---
+++ Assignment 7 Auction system/Item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7_Auction_system
+{
+    class Item
+    {
+        private string description;
+        private double reserveprice;
+        private int bidderID;
+        private double highestbid;
+
+        public Item(string description, double reserveprice)
+        {
+            this.description = description;
+            this.reserveprice = reserveprice;
+        }
+
+        public string Getdescription()
+        {
+            return description;
+        }
+
+        public double Getreserveprice()
+        {
+            return reserveprice;
+        }
+
+        public double Gethighestbid()
+        {
+            return highestbid;
+        }
+
+        public int GetbidderID()
+        {
+            return bidderID;
+        }
+        public bool Sethighestbid(int bidderID, double newbid)
+        {
+            if(newbid > highestbid && newbid > reserveprice)
+            {
+                highestbid = newbid;
+                this.bidderID = bidderID;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("That is lower than the highest bid");
+                return false;
+            }
+        }
+
+        public void SetbidderID(int newID)
+        {
+            bidderID = newID;
+        }
+    }
+}
---
+++ Assignment 7 Auction system/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Assignment_7_Auction_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items = new Item[5];
+
+            items[0] = new Item("Mobile Phone", 5000);
+            items[1] = new Item("Walkie Talkie", 1000);
+            items[2] = new Item("Headphones", 500);
+            items[3] = new Item("Earphones", 500);
+            items[4] = new Item("Screen Protector", 200);
+
+
+
+            for(int i = 1; i < items.Length; i++)
+            {
+                if(items[i] != null)
+                {
+                    Console.WriteLine("Description: " + items[i].Getdescription() + "  Reserve Price: " + items[i].Getreserveprice());
+                }
+            }
+
+            static void BidOnItem(int bidderID, double newbid, string item)
+            {
+                
+            }
+            
+        }
+    }
+}
